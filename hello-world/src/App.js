e8bb4a69b80f5514cd0e3edfda76a43b0aed233d
hactuallybenji <mrbenamiller@gmail.com>
2023-09-12 20:35:06 -0700

    Adds prettier

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Hello World! This is a React app.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-40 container">
+      <h1 className="text-3xl font-bold underline">React App</h1>
+      <p className="text-lime-400">This is the react application.</p>
     </div>
   );
 }
