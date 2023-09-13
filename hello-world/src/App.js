35b7f4d835e817c77d0a8a42a54cdf95479fa03b
hactuallybenji <mrbenamiller@gmail.com>
2023-09-13 07:37:28 -0700

    Add prettier dependency for off-VSC code

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="m-40 container">
       <h1 className="text-3xl font-bold underline">React App</h1>
       <p className="text-lime-400">This is the react application.</p>
+      <p className="text-lime-400">This is a test</p>
     </div>
   );
 }
